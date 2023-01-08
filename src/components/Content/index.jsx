4c0033ffac7c1b985e8c9b6fcf5e453c2a37a2fe
Krzysztof Dragan <Krzysztof.Dragan@stepstone.com>
2023-01-08 17:07:32 +0100

    update gitignore

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -2,11 +2,14 @@ import {ContentWrapper} from "./styled";
 import {GenerationForm} from "../GenerationForm";
 import {AnalyticsDashboard} from "../AnalyticsDashboard";
 import {useState} from "react";
+import axios from "axios";
 
 export const Content = () => {
     const [showSuccess, setShowSuccess] = useState(false);
 
     const initializeGenerator = (dataGenerationRequest) => {
+        axios.post('/api/initialize', dataGenerationRequest)
+            .then(response => console.log(response))
         setShowSuccess(true)
     }
 
--- src/components/GenerationForm/index.jsx
+++ src/components/GenerationForm/index.jsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import CardContent from '@mui/material/CardContent';
 import {GenerationFormWrapper} from "./styled";
 import {FormStepper} from "../FormStepper";
-import {useState} from "react";
 import {SuccessToast} from "../SuccessToast";
 
 export const GenerationForm = ({showSuccess, initializeGenerator}) => {
